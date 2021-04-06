25f0c917613d93737db73faddd93efcf1dcb08ab
Konstantin <leader_trident@mail.ru>
2021-04-06 11:32:28 +0300

    5. Добавил grid в index.html

--- src/main/java/ru/new_digital_world/web_app/NewDigitalWorldApplication.java
+++ src/main/java/ru/new_digital_world/web_app/NewDigitalWorldApplication.java
@@ -2,8 +2,6 @@ package ru.new_digital_world.web_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication()
 public class NewDigitalWorldApplication{
--- src/main/java/ru/new_digital_world/web_app/controller/MainController.java
+++ src/main/java/ru/new_digital_world/web_app/controller/MainController.java
@@ -3,7 +3,6 @@ package ru.new_digital_world.web_app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import ru.new_digital_world.web_app.service.PointService;
 
 @Controller
